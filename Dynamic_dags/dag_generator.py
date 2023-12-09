70c42d202cb3a7bd57c87585142ae819fdb9bb7d
FahadAsifGit <55692761+FahadAsifGit@users.noreply.github.com>
2023-12-09 20:39:46 +0530

    Update dag_generator.py

--- Dynamic_dags/dag_generator.py
+++ Dynamic_dags/dag_generator.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import dagfactory
 
-dag_factory = dagfactory.DagFactory("./dag_config.yaml")
+dag_factory = dagfactory.DagFactory("/home/runner/work/AF_REPO/AF_REPO/Dynamic_dags/dag_config.yaml")
 
 dag_factory.clean_dags(globals())
 dag_factory.generate_dags(globals())
